8397aa85260028b8578401cb05670df409528cdd
Sergio Torres <sergio.torres@idr.edu.mx>
2022-09-11 23:48:04 -0500

    feat: find matches

--- myscript.js
+++ myscript.js
@@ -1,1 +1,17 @@
 console.log("My script");
+const materiasBase = ["Ingles", "Química", "Física", "Historia", "Mecánica"];
+
+function mostrarCoincidencias(){
+    console.log("click on button");
+    //console.log(obtenerMaterias());
+    alert("Coincidencias: " +  buscaCoincidencias( obtenerMaterias(), materiasBase))
+}
+
+function buscaCoincidencias(materias, materiasBase){
+    return materias.filter( materia=> materiasBase.includes(materia));
+}
+
+function obtenerMaterias(){
+    const inputs = document.getElementById("materias").value;
+    return inputs.replace(" ","").split(",");
+}
